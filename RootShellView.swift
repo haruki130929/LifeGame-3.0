771d3d67f1e66a77d9ca2715240c37539c174685
haruki130929 <harukiyang122@gmail.com>
2026-02-21 23:43:12 +0800

    更新 RootShellView.swift

--- RootShellView.swift
+++ RootShellView.swift
@@ -1,28 +1,10 @@
 import SwiftUI
 
 struct RootShellView: View {
-    
-    // 全域共用 stores
-    @StateObject private var theme = ThemeStore()
-    @StateObject private var calendarStore = CalendarStore()
-    @StateObject private var calendarSettings = CalendarSettingsStore()
-    
-    @StateObject private var fab = FabStore()
-    
-    @StateObject private var wishStore = WishStore()
-    @StateObject private var ledgerStore = LedgerStore()
-    
     var body: some View {
+        // 讓 HomeRootView 裡的 NavigationLink 有一個導航容器可以 push
         NavigationStack {
             HomeRootView()
         }
-        .environmentObject(theme)
-        .environmentObject(calendarStore)
-        .environmentObject(calendarSettings)
-        
-        .environmentObject(fab)
-        
-        .environmentObject(wishStore)
-        .environmentObject(ledgerStore)
     }
 }
