5d12a77af98bab920b1c5fb9aa4ca3ed18ec2668
haruki130929 <harukiyang122@gmail.com>
2026-02-22 21:03:20 +0800

    更新 MyApp.swift

--- MyApp.swift
+++ MyApp.swift
@@ -2,19 +2,24 @@ import SwiftUI
 
 @main
 struct LifeGameApp: App {
-    
+    // 這些一定要跟 HomeRootView 的 @EnvironmentObject 對上
     @StateObject private var theme = ThemeStore()
     @StateObject private var calendarStore = CalendarStore()
     @StateObject private var calendarSettings = CalendarSettingsStore()
     @StateObject private var fab = FabStore()
     
+    @StateObject private var wishStore = WishStore()
+    @StateObject private var ledgerStore = LedgerStore()
+    
     var body: some Scene {
         WindowGroup {
-            RootShellView()
+            HomeRootView()
                 .environmentObject(theme)
                 .environmentObject(calendarStore)
                 .environmentObject(calendarSettings)
                 .environmentObject(fab)
+                .environmentObject(wishStore)
+                .environmentObject(ledgerStore)
         }
     }
 }
