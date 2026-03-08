5cf17766248adea159fb9a4aa7470e21c5bab451
Sonya <harukiyang122@gmail.com>
2026-03-08 20:59:26 +0800

    Initial Commit

---
+++ LifeGame/LifeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeGameApp.swift
+//  LifeGame
+//
+//  Created by sonya on 2026/3/8.
+//
+
+import SwiftUI
+
+@main
+struct LifeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
